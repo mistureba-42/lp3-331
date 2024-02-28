c49ed12c16def1fec50d62774590a96be05eb2d2
Guilherme Napoleao <guilherme.napoleao@aluno.ifsp.edu.br>
2024-02-28 14:33:44 -0300

    variaveis e constantes

---
+++ 01-intro-python/01-variaveis-constantes.py
@@ -0,0 +1,38 @@
+#identificadores
+#letras,numeros e _
+#nao pode ser palavras reservadas: if, none, etc
+#case sensitive
+
+#variaveis
+#tudo em minusculo
+#separador _
+#snake_case
+idade = 20
+pessoa_fisica = "mario"
+imposto_rende = 2200.50
+
+#inferencia de tipo
+#o tipo é definido pelo valor (litarais)
+idade = 20 #int
+pessoa_fisica = "mario" #str
+
+#constantes:
+#nao existe constante no python
+#convenção: nome em maiusculo
+PI = 3.14
+
+#comentarios de unica linha
+'''
+comentairos de
+multiplas linhas
+'''
+
+#docstring (comentario de documentação)
+# documentar classes, modulos, funções, etc
+
+def somar (numero1, numero2):
+    '''
+    Função para somar dois numeros
+    '''
+    return numero1+numero2
+
